79951ef077e3bd0cb5dc2a391707a62f7b7f83bd
Akki747 <bairapuakshaya@gmail.com>
2024-12-06 12:39:24 +0530

    Add files via upload

--- loop.java
+++ loop.java
@@ -5,9 +5,9 @@ public class loop
     {
         for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=5;i++)
+            for(int j=1;j<=5;j++)
             {
-                System.out.println(j+"");
+                System.out.println(j+" ");
             }
             System.out.println();
         }
