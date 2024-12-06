47b42d952920f7ccb5e6485299f839c5d655906c
Akki747 <bairapuakshaya@gmail.com>
2024-12-06 14:19:48 +0530

    Add files via upload

--- pattern1.java
+++ pattern1.java
@@ -5,9 +5,9 @@ public class pattern1
     {
         for(int i=1;i<=5;i++)
         {
-            for(int j=i;j<=5;j++)
+            for(int j=1;j<=5;j++)
             {
-                System.out.print("*");
+                System.out.print((char)(j+64)+" ");
             }
             System.out.println();
         }
