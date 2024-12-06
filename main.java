6c1fae29fdd1662afebdbe329bc67d345282ab2f
Akki747 <bairapuakshaya@gmail.com>
2024-12-06 13:52:37 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class main
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.println((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.println(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+        
+    
